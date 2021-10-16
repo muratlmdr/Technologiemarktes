f3b04b03efef9d3355bb1b5e5e8a959e2ad1a99a
muratlmdr <gunster6134@gmail.com>
2021-10-16 03:08:29 +0300

    pc arası aktarma

--- WEB Final Proje/main.py
+++ WEB Final Proje/main.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template, request, redirect, session
 import pymongo
 from bson.objectid import ObjectId
 #pc arası dosya aktarma denemesi vol 1
-#pc arası aktarma başarılı ++
 app = Flask(__name__)
 app.secret_key = 'GunsteR'
 
